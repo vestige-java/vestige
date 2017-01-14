c0ef734f7714dbff61ad2520ce62bb1c25c9daac
Gaël Lalire <gael.lalire@laposte.net>
2017-01-14 18:22:18 +0100

    Use extern jvm_enhancer

--- jvm_enhancer/runtime/src/main/native/windows/compile.bat
+++
@@ -1,2 +0,0 @@
-gcc -Wl,--add-stdcall-alias -I"C:\Program Files\Java\jdk1.8.0_73\include" -I"C:\Program Files\Java\jdk1.8.0_73\include\win32" WindowsShutdownHook.c -m32 -shared -o shutdownHook_x86.dll
-gcc -Wl,--add-stdcall-alias -I"C:\Program Files\Java\jdk1.8.0_73\include" -I"C:\Program Files\Java\jdk1.8.0_73\include\win32" WindowsShutdownHook.c -m64 -shared -o shutdownHook_amd64.dll
