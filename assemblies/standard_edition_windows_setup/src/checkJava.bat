2a1cdef4e4a16f26c2d36b31ec40ba994c082883
Gaël Lalire <gael.lalire@gaellalire.fr>
2020-03-15 16:12:11 +0100

    update check java with VESTIGE_JAVA

--- assemblies/standard_edition_windows_setup/src/checkJava.bat
+++ assemblies/standard_edition_windows_setup/src/checkJava.bat
@@ -1,26 +1,32 @@
 @echo off
 
-setlocal
+setlocal enableextensions enabledelayedexpansion
 
-set DIRNAME=%~dp0
-if "%DIRNAME:~-1%" == "\" (set DIRNAME=%DIRNAME:~0,-1%)
+set "DIRNAME=%~dp0"
+if [%DIRNAME:~-1%] == [\] set "DIRNAME=%DIRNAME:~0,-1%"
+
+if defined VESTIGE_JAVA goto :vestige_java_set
 
 if defined JAVA goto :java_found
 
-for %%I in (java.exe) do set JAVA=%%~$PATH:I
-if defined JAVA goto :java_found
+for %%I in (java.exe) do set "VESTIGE_JAVA=%%~$PATH:I"
+if defined VESTIGE_JAVA goto :vestige_java_set
 
 if defined JAVA_HOME goto :java_home_found
 
-echo Unable to start a JVM : %%JAVA%% is not set and java.exe is not in PATH and %%JAVA_HOME%% is not set
+echo Unable to start a JVM : %%VESTIGE_JAVA%% and %%JAVA%% are not set and java.exe is not in PATH and %%JAVA_HOME%% is not set
 exit /B 1
 
 :java_home_found
-set JAVA=%JAVA_HOME%\bin\java.exe
+set "VESTIGE_JAVA=%JAVA_HOME%\bin\java.exe"
+goto :vestige_java_set
 
 :java_found
+set "VESTIGE_JAVA=%JAVA%"
 
-"%JAVA%" CheckJava || exit /B 2
+:vestige_java_set
+
+"%VESTIGE_JAVA%" CheckJava || exit /B 2
 
 echo "" > "%DIRNAME%\java_check.txt"
 
