930270a98f01d4a57344f5d4f418659227cae0b9
Gaël Lalire <gael.lalire@gaellalire.fr>
2020-11-26 01:16:12 +0100

    Fix module-info

--- admin/telnet/src/main/java/module-info.java
+++ admin/telnet/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module fr.gaellalire.vestige.admin.telnet {
     
     requires slf4j.api;
 
+    requires fr.gaellalire.vestige.job;
+
     requires fr.gaellalire.vestige.admin.command;
 
     exports fr.gaellalire.vestige.admin.telnet;
